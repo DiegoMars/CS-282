2ecb53a95fc8cb4e7aedb70fd8fcfe6600a4e7b2
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-09-10 20:13:56 -0700

    Finished divisibility package

--- homeWorkOne/src/main/java/divisibility/Divisibility.java
+++ homeWorkOne/src/main/java/divisibility/Divisibility.java
@@ -16,10 +16,22 @@ package divisibility;
 //Enter the divisor: 0
 //10 is not divisible by 0
 
-
+import java.util.Scanner;
 public class Divisibility {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the dividend: ");
+        String dividend = input.nextLine();
+        System.out.print("Enter the divisible: ");
+        String divisible = input.nextLine();
 
+        if (Integer.parseInt(divisible) == 0){
+            System.out.printf("%s is divisible by %s", dividend, divisible);
+        } else if (Integer.parseInt(dividend) / Integer.parseInt(divisible) == 1){
+            System.out.printf("%s is not divisible by %s", dividend, divisible);
+        } else {
+            System.out.printf("%s is divisible by %s", dividend, divisible);
+        }
     }
 }
